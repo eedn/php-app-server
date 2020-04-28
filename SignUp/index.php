71b8044fbfc57082f2833710c1d1478be595f653
unknown <fafa6678@naver.com>
2020-04-28 23:34:55 +0900

    Complete Sign Up

---
+++ SignUp/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<meta charset="utf-8" />
+<form method='post' action='signup.php'>
+<table>
+<tr>
+	<td>아이디</td>
+	<td><input type='text' name='user_id' tabindex='1'/></td>
+	<td rowspan='3'><input type='submit' tabindex='4' value='가입신청' style='height:75px'/></td>
+</tr>
+<tr>
+	<td>비밀번호</td>
+	<td><input type='password' name='pw' tabindex='2'/></td>
+</tr>
+<tr>
+	<td>이름</td>
+	<td><input type='text' name='name' tabindex='3'/></td>
+</tr>
+</table>
+</form>
+<a href="../Login"><button>로그인 화면으로</button></a>
---
+++ SignUp/signup.php
@@ -0,0 +1,51 @@
+<?php
+    //POST로 user_id가 없거나 pw나 name이 없을 경우 예외 처리
+    if(!isset($_POST['user_id']) || !isset($_POST['pw']) || !isset($_POST['name'])) exit;
+
+    header('content-type: text/html; charset=utf-8');
+    // 데이터베이스 접속 문자열. (db위치, 유저 이름, 비밀번호)
+    $connect=mysqli_connect( "localhost:3306", "tennis", "tennis") or  
+    die( "SQL server에 연결할 수 없습니다.");
+
+
+    mysqli_query($connect,"SET NAMES UTF8");
+    // 데이터베이스 선택
+    mysqli_select_db($connect,"tennis");
+
+    // 세션 시작
+    session_start();
+
+    $id = $_POST['user_id']; //POST 들어온 id,pw,name을 설정
+    $pw = $_POST['pw'];
+    $name = $_POST['name'];
+    
+    $sql = "SELECT user_id FROM tennis_user WHERE user_id = '$id'";
+    $result = mysqli_query($connect, $sql) or die("에러 발생1");
+
+    $count = mysqli_num_rows($result);
+
+    if($count>=1){ // 아이디 중복 체크
+        echo "중복된 아이디입니다!";
+        exit;
+    }
+
+    $sql = "INSERT INTO tennis_user (user_id, pw, name) VALUES ('$id', '$pw', '$name')";
+    $result = mysqli_query($connect, $sql) or die("에러 발생2");
+
+    // result of sql query
+    if($result)
+    {
+        echo "회원가입 완료!";
+        $_SESSION['user_id'] = $id;
+        $_SESSION['name'] = $name;
+        header('location:../Room/');
+    }
+    else
+    {
+        die("에러 발생3");
+        // echo mysqli_errno($connect);
+    }
+
+    mysqli_close($connect);
+
+?>
