853d6e40d956917cfe4eafb307dccec5cb703467
hyeonwoo <fafa6678@naver.com>
2020-04-30 09:31:31 +0900

    Complete Web tester

--- RoomList/index.php
+++ RoomList/index.php
@@ -2,12 +2,16 @@
 <?php
 
     // 세션 시작
-    session_start();
+    // session_start(); //include roomlist.php 에도 session_start 있음
+    include('roomlist.php');
 
     $id = $_SESSION['user_id'];
     $name = $_SESSION['name'];
 
-    echo "이름은 {$name}이고 아이디는 {$id}이야.<br/>";
+    echo "<br/><br/>이름은 {$name}이고 아이디는 {$id}이야.<br/>";
 
 ?>
-<a href="roomlist.php"><button>룸 목록 보기</button></a><br/>
+
+<form method='post' action='../Room'>
+    room_id 입력 : <input type='text' name='room_id'/> <input type='submit' value='room 입장'/>
+</form>
