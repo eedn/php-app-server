34681cf7994c3e834cd3954c28776a97f531b31b
hyeonwoo <fafa6678@naver.com>
2020-04-30 11:57:12 +0900

    fixed error -form

--- VideoUpload/index.php
+++ VideoUpload/index.php
@@ -1,15 +1,27 @@
+<!DOCTYPE html>
+<meta charset="utf-8" />
+<a href="../Logout"><button>로그아웃</button></a><br/>
 <?php
- 
-	if($_SERVER['REQUEST_METHOD']=='POST'){
-		$file_name = $_FILES['myFile']['name'];
-		$file_size = $_FILES['myFile']['size'];
-		$file_type = $_FILES['myFile']['type'];
-		$temp_name = $_FILES['myFile']['tmp_name'];
-				
-		$location = "uploads/";
-			
-		move_uploaded_file($temp_name, $location.$file_name);
-		echo "http://localhost/VideoUpload/uploads/".$file_name;
-	}else{
-		echo "Error";
-	}
+
+    // 세션 시작
+    session_start();
+
+    $id = $_SESSION['user_id'];
+    $name = $_SESSION['name'];
+
+    echo "이름은 {$name}.<br/>";
+
+?>
+<form enctype="multipart/form-data" method='post' action='videoupload.php'>
+<table>
+<tr>
+	<td>room_id : </td>
+	<td><input type='text' name='room_id' tabindex='1'/></td>
+	<td rowspan='2'><input type='submit' tabindex='3' value='전송' style='height:50px'/></td>
+</tr>
+<tr>
+	<td>파일</td>
+	<td><input type='file'' name='myFile' tabindex='2'/></td>
+</tr>
+</table>
+</form>
